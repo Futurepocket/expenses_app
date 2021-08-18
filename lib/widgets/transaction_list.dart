4767103afd0fa89390278a998348a68d6f7c3663
Marco Carvalhana <marco@glyph.pt>
2021-08-18 14:30:32 +0100

    .

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -35,7 +35,7 @@ class TransactionList extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
-                      color: Colors.purple,
+                      color: Theme.of(context).primaryColor,
                     ),
                   ),
                 ),
